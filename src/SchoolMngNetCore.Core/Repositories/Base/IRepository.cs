8756fcc9ffd57fd51ee0464a3a9993e023c816ab
gokalpi <gokalpi@gmail.com>
2020-02-01 16:46:04 +0300

    Added repository interfaces

---
+++ src/SchoolMngNetCore.Core/Repositories/Base/IRepository.cs
@@ -0,0 +1,43 @@
+﻿using SchoolMngNetCore.Core.Entities.Base;
+using SchoolMngNetCore.Core.Specifications.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SchoolMngNetCore.Core.Repositories.Base
+{
+    public interface IRepository<T> : IRepository<T, int> where T : IEntity<int>
+    {
+    }
+
+    public interface IRepository<T, TId> where T : IEntity<TId>
+    {
+        Task<T> GetByIdAsync(TId id);
+
+        Task<IReadOnlyList<T>> ListAllAsync();
+
+        Task<IReadOnlyList<T>> GetAsync(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeString = null,
+            bool disableTracking = true);
+
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<Expression<Func<T, object>>> includes = null,
+            bool disableTracking = true);
+
+        Task<int> CountAsync(ISpecification<T> spec);
+
+        Task<T> AddAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
---
+++ src/SchoolMngNetCore.Core/Repositories/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿using SchoolMngNetCore.Core.Entities;
+using SchoolMngNetCore.Core.Repositories.Base;
+
+namespace SchoolMngNetCore.Core.Repositories
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+    }
+}
