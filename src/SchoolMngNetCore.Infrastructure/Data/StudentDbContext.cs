bbdd5a425f8bb887999fec2d3f1758bb3ff8aae4
gokalpi <gokalpi@gmail.com>
2020-02-01 18:35:32 +0300

    Custom DbContext implemented

---
+++ src/SchoolMngNetCore.Infrastructure/Data/StudentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolMngNetCore.Core.Entities;
+using SchoolMngNetCore.Infrastructure.Identity;
+
+namespace SchoolMngNetCore.Infrastructure.Data
+{
+    public class StudentDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
