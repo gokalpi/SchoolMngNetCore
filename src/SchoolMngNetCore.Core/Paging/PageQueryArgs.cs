f381e32d6b2af9ec903f618cb5301ac27c0c11b9
gokalpi <gokalpi@gmail.com>
2020-02-01 16:45:12 +0300

    Added paging core support

---
+++ src/SchoolMngNetCore.Core/Paging/PageQueryArgs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SchoolMngNetCore.Core.Paging
+{
+    public class PageQueryArgs
+    {
+        /// <summary>
+        /// Page index
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// Page size
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// Sorting options
+        /// </summary>
+        public List<SortingOption> SortingOptions { get; set; }
+
+        /// <summary>
+        /// Filtering options
+        /// </summary>
+        public List<FilteringOption> FilteringOptions { get; set; }
+    }
+}
