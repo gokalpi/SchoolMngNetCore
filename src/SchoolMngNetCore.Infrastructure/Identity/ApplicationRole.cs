57cf1edf4d45022d0444ef213c5ea89a20259369
gokalpi <gokalpi@gmail.com>
2020-02-01 18:34:42 +0300

    Custom identity entities added to new Infrastructure project

---
+++ src/SchoolMngNetCore.Infrastructure/Identity/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SchoolMngNetCore.Core.Entities.Base;
+
+namespace SchoolMngNetCore.Infrastructure.Identity
+{
+    public class ApplicationRole : IdentityRole<int>, IEntity<int>
+    {
+    }
+}
---
+++ src/SchoolMngNetCore.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SchoolMngNetCore.Core.Entities.Base;
+
+namespace SchoolMngNetCore.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<int>, IEntity<int>
+    {
+    }
+}
