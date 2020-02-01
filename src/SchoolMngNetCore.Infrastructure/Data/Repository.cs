67c3e9169bd9dede9f1c98264b42124044bbb34f
gokalpi <gokalpi@gmail.com>
2020-02-01 18:35:57 +0300

    Specification evaluator added

---
+++ src/SchoolMngNetCore.Infrastructure/Data/Repository.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolMngNetCore.Core.Entities.Base;
+using SchoolMngNetCore.Core.Repositories.Base;
+using SchoolMngNetCore.Core.Specifications.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SchoolMngNetCore.Infrastructure.Data
+{
+    public class Repository<T> : Repository<T, int> where T : class, IEntity<int>
+    {
+        public Repository(StudentDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+    public class Repository<T, TId> : IRepository<T, TId> where T : class, IEntity<TId>
+    {
+        protected readonly StudentDbContext _dbContext;
+
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(StudentDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public virtual async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeString = null,
+            bool disableTracking = true)
+        {
+            var query = disableTracking ? _dbSet.AsNoTracking() : _dbSet;
+
+            if (!string.IsNullOrWhiteSpace(includeString))
+            {
+                query = query.Include(includeString);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<Expression<Func<T, object>>> includes = null,
+            bool disableTracking = true)
+        {
+            var query = disableTracking ? _dbSet.AsNoTracking() : _dbSet;
+
+            if (includes != null)
+            {
+                query = includes.Aggregate(query, (current, include) => current.Include(include));
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(TId id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T, TId>.GetQuery(_dbSet.AsQueryable(), spec);
+        }
+    }
+}
---
+++ src/SchoolMngNetCore.Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolMngNetCore.Core.Entities.Base;
+using SchoolMngNetCore.Core.Specifications.Base;
+using System.Linq;
+
+namespace SchoolMngNetCore.Infrastructure.Data
+{
+    public class SpecificationEvaluator<T> : SpecificationEvaluator<T, int> where T : class, IEntity<int>
+    {
+    }
+
+    public class SpecificationEvaluator<T, TId> where T : class, IEntity<TId>
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> specification)
+        {
+            var query = inputQuery;
+
+            // modify the IQueryable using the specification's criteria expression
+            if (specification.Criteria != null)
+            {
+                query = query.Where(specification.Criteria);
+            }
+
+            // Includes all expression-based includes
+            query = specification.Includes.Aggregate(query,
+                                    (current, include) => current.Include(include));
+
+            // Include any string-based include statements
+            query = specification.IncludeStrings.Aggregate(query,
+                                    (current, include) => current.Include(include));
+
+            // Apply ordering if expressions are set
+            if (specification.OrderBy != null)
+            {
+                query = query.OrderBy(specification.OrderBy);
+            }
+            else if (specification.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(specification.OrderByDescending);
+            }
+
+            if (specification.GroupBy != null)
+            {
+                query = query.GroupBy(specification.GroupBy).SelectMany(x => x);
+            }
+
+            // Apply paging if enabled
+            if (specification.IsPagingEnabled)
+            {
+                query = query.Skip(specification.Skip)
+                             .Take(specification.Take);
+            }
+            return query;
+        }
+    }
+}
